2ac08677755c92d2b0689cfdbc2b62823a58b616
Gabriel Sugai <gabrielsugai@gmail.com>
2021-03-18 16:06:46 -0300

    Adiciona validacao de dados

--- app/models/promotion.rb
+++ app/models/promotion.rb
@@ -1,2 +1,6 @@
 class Promotion < ApplicationRecord
+  validates :name, :discount_rate, :coupon_quantity, :expiration_date, 
+            :code, presence: { message: 'não pode ficar em branco'}
+  
+  validates :code, :name, uniqueness: { message: 'deve ser único'}
 end
