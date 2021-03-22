b123f563b21451f95680e6370dfef86452942093
Gabriel Sugai <gabrielsugai@gmail.com>
2021-03-22 01:28:44 -0300

    Categoria deve possuir nome e codigo unicos

--- app/models/product_category.rb
+++ app/models/product_category.rb
@@ -1,2 +1,4 @@
 class ProductCategory < ApplicationRecord
+  validates :name, :code, presence: { message: 'não pode ficar em branco'}
+  validates :name, :code, uniqueness: { message: 'deve ser único'}
 end
--- test/models/product_category_test.rb
+++ test/models/product_category_test.rb
@@ -1,7 +1,21 @@
 require "test_helper"
 
 class ProductCategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'attributes cannot be blank' do
+    category = ProductCategory.new
+
+    refute category.valid?
+    assert_includes category.errors[:name], 'não pode ficar em branco'
+    assert_includes category.errors[:code], 'não pode ficar em branco'
+  end
+
+  test 'code and name must be uniq' do
+    ProductCategory.create!(name: 'Produto Teste', code: 'TESTE')
+    category = ProductCategory.new(name: 'Produto Teste', code: 'TESTE')
+
+    refute category.valid?
+    assert_includes category.errors[:name], 'deve ser único'
+    assert_includes category.errors[:code], 'deve ser único'
+  end
 end
+
